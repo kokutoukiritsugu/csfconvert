c2faf253d2bf8699eb4f6fda928da7f767e1e268
kokutoukiritsugu <kokutoukiritsugu@gmail.com>
2016-01-23 13:21:36 +0800

    delete some bats. add commit to renameHEXtoName.bat

--- javaversion/renameHEXtoName.bat
+++ javaversion/renameHEXtoName.bat
@@ -1,4 +1,10 @@
 @echo off
+:: some protect mix file has no file name. but it can unprotect.
+:: find some normal mix file , use XCC Mixer open it,
+:: double click into mix file, then click any file.
+:: in right window, ID is the filename.
+:: So, If you have a lot time to find HEX correspond filename, you can get the real name.
+
 rename 2D2F67F2 stringtable98.csf
 rename F40F755E stringtable97.csf
 rename 38A575C0 stringtable96.csf
