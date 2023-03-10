a1127f3cfdd7f082fdfc3d2ac5a38c618f08b37c
kokutoukiritsugu <kokutoukiritsugu@gmail.com>
2023-03-10 22:34:35 +0800

    befor to json

--- pythonversion/csftoini.py
+++ pythonversion/csftoini.py
@@ -2,67 +2,76 @@
 import struct
 import configobj
 
-csf_name = "ra2.csf"
-ini_name = "ra2.csf.ini"
-encoding_inifile = 'utf-8'
-encoding_csf_str = 'utf-16le'
-open(ini_name, 'w').close()
-csffile = open(csf_name, mode='rb')
-inifile = configobj.ConfigObj(ini_name, encoding=encoding_inifile)
-inifile.newlines = "\r\n"
+CSF = "ra2.csf"
+INI = "ra2.csf.ini"
 
-bHEADcsf, unknowIntVaule1, strCount1, strCount2 = struct.unpack("<4sLLL", csffile.read(16))
-unknowIntVaule2, unknowIntVaule3 = struct.unpack("<LL", csffile.read(8))
-sHEADcsf = bHEADcsf.decode("ascii")
+encoding_INI = 'utf-8'
+encoding_CSF_str = 'utf-16le'
 
-inifile[csf_name] = {}
-inifile[csf_name]["head"] = sHEADcsf
-inifile[csf_name]["unknowIntVaule1"] = unknowIntVaule1
-inifile[csf_name]["strCount1"] = strCount1
-inifile[csf_name]["strCount2"] = strCount2
-inifile[csf_name]["unknowIntVaule2"] = unknowIntVaule2
-inifile[csf_name]["unknowIntVaule3"] = unknowIntVaule3
+open(INI, 'w').close()
 
-print(inifile[csf_name])
+fCSF = open(CSF, mode='rb')
+fINI = configobj.ConfigObj(INI, encoding=encoding_INI)
+fINI.newlines = "\r\n"
+
+bHeadCSF, iUnknowVaule1, iStringCount1, iStringCount2 = struct.unpack("<4sLLL", fCSF.read(16))
+iUnknowVaule2, iUnknowVaule3 = struct.unpack("<LL", fCSF.read(8))
+sHeadCSF = bHeadCSF.decode("ascii")
+
+fINI[CSF] = {}
+fINI[CSF]["HeadCSF"] = sHeadCSF
+fINI[CSF]["UnknowVaule1"] = iUnknowVaule1
+fINI[CSF]["StringCount1"] = iStringCount1
+fINI[CSF]["StringCount2"] = iStringCount2
+fINI[CSF]["UnknowVaule2"] = iUnknowVaule2
+fINI[CSF]["UnknowVaule3"] = iUnknowVaule3
+
+print(fINI[CSF])
 
 print()
 
-txtstring = 0
-while not txtstring == strCount1:
-    strHead, strUnknowVaule, strLong = struct.unpack("<4sLL", csffile.read(12))
-    bLBL = struct.unpack("<%ds" % strLong, csffile.read(strLong))
-    sLBL = bLBL[0].decode("ascii")
-    print(sLBL)
+n = 0
+while not n == iStringCount1:
+    bHeadStr, iUnknowVaule4, iStringLength = struct.unpack("<4sLL", fCSF.read(12))
+    sHeadStr = bHeadStr.decode("ascii")
+
+    bStringKey = struct.unpack("<%ds" % iStringLength, fCSF.read(iStringLength))
+    sStringKey = bStringKey[0].decode("ascii")
+    print(sStringKey)
     print()
-    inifile[sLBL] = {}
+    fINI[sStringKey] = {}
 
-    bTYPE = struct.unpack("<4s", csffile.read(4))
-    print(bTYPE)
+    bStringType = struct.unpack("<4s", fCSF.read(4))
+    print(bStringType)
     print()
-    if bTYPE[0] == b' RTS':
-        inifile[sLBL]["strMode"] = "STR"
-    if bTYPE[0] == b'WRTS':
-        inifile[sLBL]["strMode"] = "STRW"
+    if bStringType[0] == b' RTS':
+        fINI[sStringKey]["StringType"] = "STR"
+    if bStringType[0] == b'WRTS':
+        fINI[sStringKey]["StringType"] = "STRW"
 
-    bString1Length = struct.unpack("<L", csffile.read(4))
-    iString1Length = bString1Length[0] * 2
-    bString1raw = struct.unpack("<%ds" % (bString1Length[0] * 2), csffile.read(bString1Length[0] * 2))
-    bString1 = b''
-    for b in bString1raw[0]:
-        bString1 = bString1 + (b ^ 0xff).to_bytes(1, "little")
-    sString1 = bString1.decode(encoding_csf_str)
-    print(sString1)
-    print()
-    inifile[sLBL]["string"] = "%s" % sString1
+    if bStringType[0]:
+        bString1ValueLength = struct.unpack("<L", fCSF.read(4))
+        iString1ValueLength = bString1ValueLength[0] * 2
+        print(bString1ValueLength)
+        bString1ValueRaw = struct.unpack("<%ds" % (bString1ValueLength[0] * 2), fCSF.read(bString1ValueLength[0] * 2))
+        bString1Value = b''
+        for b in bString1ValueRaw[0]:
+            bString1Value = bString1Value + (b ^ 0xff).to_bytes(1, "little")
+        sString1Value = bString1Value.decode(encoding_CSF_str)
+        print(sString1Value)
+        print()
+        fINI[sStringKey]["String1Value"] = "%s" % sString1Value
 
-    if bTYPE[0] == b'WRTS':
-        bString2Length = struct.unpack("<L", csffile.read(4))
-        bString2 = struct.unpack("<%ds" % bString2Length[0], csffile.read(bString2Length[0]))
-        sString2 = bString2[0].decode("ascii")
-        print(sString2)
-        inifile[sLBL]["ExtraString"] = "%s" % sString2
+    if bStringType[0] == b'WRTS':
+        bString2ValueLength = struct.unpack("<L", fCSF.read(4))
+        print(bString2ValueLength)
+        bString2Value = struct.unpack("<%ds" % bString2ValueLength[0], fCSF.read(bString2ValueLength[0]))
+        print(bString2Value)
+        sString2Value = bString2Value[0].decode("ascii")
+        print(sString2Value)
+        fINI[sStringKey]["String2Value"] = "%s" % sString2Value
 
-    txtstring += 1
+    n += 1
 
 
-inifile.write()
+fINI.write()
